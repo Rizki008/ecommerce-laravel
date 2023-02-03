ea67604e067336f4619ebbaa478460047804658a
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-02-03 14:27:16 +0700

    membuat validasi api update kategori

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
@@ -106,7 +107,30 @@ class CategoryController extends Controller
      */
     public function update(Request $request, Category $category)
     {
-        $category->update($request->all());
+        $validator = Validator::make($request->all(), [
+            'nama_kategori' => 'required',
+            'deskripsi' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                $validator->errors(), 422
+            ]);
+        }
+
+        $input = $request->all();
+
+        if ($request->has('gambar')) {
+            File::delete('uploads/' . $category->gambar);
+            $gambar = $request->file('gambar');
+            $nama_gambar = time() . rand(1, 9) . '.' . $gambar->getClientOriginalExtension();
+            $gambar->move('uploads', $nama_gambar);
+            $input['gambar'] = $nama_gambar;
+        } else {
+            unset($input['gambar']);
+        }
+
+        $category->update($input);
 
         return response()->json([
             'message' => 'Success',
