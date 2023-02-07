81d4663f68e946cf02d84d97dcc54cb5c70bbf31
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-02-07 12:05:05 +0700

    menambahkan api pada auth construct

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -12,7 +12,7 @@ class CategoryController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth', ['except' => 'index']);
+        $this->middleware('auth:api', ['except' => 'index']);
     }
     /**
      * Display a listing of the resource.
--- app/Http/Controllers/SliderController.php
+++ app/Http/Controllers/SliderController.php
@@ -11,7 +11,7 @@ class SliderController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth', ['except' => 'index']);
+        $this->middleware('auth:api', ['except' => 'index']);
     }
     /**
      * Display a listing of the resource.
