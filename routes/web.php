1af091bf02152e6e16c2601b8d1ba9e9c647656e
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-02-17 11:01:57 +0700

    menambahkan route get kategori

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,4 +27,11 @@ Route::get('/', function () {
 Route::get('login', [AuthController::class, 'index'])->name('login');
 Route::post('login', [AuthController::class, 'login']);
 Route::get('logout', [AuthController::class, 'logout']);
+
+//kategori
+// Route::get('/kategori', [CategoryController::class], 'list');
+Route::get('/kategori', [App\Http\Controllers\CategoryController::class, 'list'])->name('kategori');
+
+
+
 Route::get('/dashboard', [DashboardController::class, 'index']);
